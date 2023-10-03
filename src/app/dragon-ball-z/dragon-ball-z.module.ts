a134d90816201ad5f3b367216b927213fa5d0905
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-03 14:04:26 -0500

    [+] DbzListComponent: Listado de personajes de Dragon Ball Z

--- src/app/dragon-ball-z/dragon-ball-z.module.ts
+++ src/app/dragon-ball-z/dragon-ball-z.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
-import { DbzListComponent } from './pages/dbz-list/dbz-list.component';
-
+import { DbzListComponent } from './components/dbz-list/dbz-list.component';
 
 @NgModule({
   declarations: [
