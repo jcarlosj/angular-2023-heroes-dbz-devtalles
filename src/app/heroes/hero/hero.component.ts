093b1bfadf0d158391766e655156470a8efe9b85
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-03 09:13:44 -0500

    [+] HeroComponent: HTML, interpolacion de datos y CSS

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
-
+  public name: string = 'Peter Parker';
+  public alterego: string = 'Spiderman';
+  public age:  number = 27;
 }
